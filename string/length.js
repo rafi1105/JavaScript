05162ab53b3658bc4f27cfb84a05c38ea4105307
rafi1105 <111092930+rafi1105@users.noreply.github.com>
2023-01-06 12:32:45 +0600

    rk

---
+++ string/length.js
@@ -0,0 +1,7 @@
+function getCharCount(str) {
+    return str.length;
+}
+
+// Sample usage - do not modify
+console.log(getCharCount("Sam")); // 3
+console.log(getCharCount("Alex 123")); // 8
---
+++ string/run.js
@@ -0,0 +1,19 @@
+
+console.log(getCharCount("Charley is here")); // 15
+
+/*"nice!".length;
+discription.length; */
+let text = "Hello World";
+text.length; // 11
+// "BLUE".toLowerCase(); // "blue";
+//"red".toUpperCase(); // "RED";
+
+/* function getCharCount(str) {
+    return str.length;
+}
+
+// Sample usage - do not modify
+console.log(getCharCount("Sam")); // 3
+console.log(getCharCount("Alex 123")); // 8
+console.log(getCharCount("Charley is here")); // 15
+*/
