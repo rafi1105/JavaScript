8e672dccb33a7f6a9cd4a5e82c9162b32492f1f9
Rafi_kabir <111092930+rafi1105@users.noreply.github.com>
2025-08-07 04:34:45 +0600

    Dom

--- Dom/learn_DOM/index.js
+++ Dom/learn_DOM/index.js
@@ -1,25 +1,27 @@
+//  style
+document.querySelector("p").style.color= "red"
+// check box manuculation
 let checkBox= document.querySelectorAll('input[type="checkbox"]')
 checkBox.forEach(element => {
     element.checked=false;
 });
 
-function insetSafe(element, userName){
-    element.innerHTML = userName + " rafi";
-}
-
-let e1 = document.querySelector('.safeInsert');
-let userNameInput = document.querySelector('input[type="text"]');
-let userNameValue = userNameInput.value; // Get the 
 // input value
-document.querySelector("p").style.color= "red"
 // Update content when input changes
+let userNameInput = document.querySelector('#userText');
+
+let userNameValue = userNameInput.value; // Get the 
 userNameInput.addEventListener('input', function() {
     insetSafe(e1, this.value);
 });
 
 // Initial call with current value
+let e1 = document.querySelector('.safeInsert');
+function insetSafe(element, userName){
+    element.innerHTML = userName + " rafi kabir";
+}
 insetSafe(e1, userNameValue);
-
+// make dark mode
 const card = document.querySelector(".card")
  card.classList.add("active")
  card.classList.toggle('active');
@@ -38,4 +40,41 @@ const darkMode = (()=>{
 
 })
 
-// todo 
+// attribute selector
+let attribute=document.querySelector(".attri");
+attribute.setAttribute('href','https://www.youtube.com/')
+console.log("Get attribute : ",attribute.getAttribute('href'))
+console.log("Remove attribute: ", attribute.removeAttribute('href')) // undefined
+console.log(attribute.getAttribute('href')) // null 
+// try somethings
+
+ let show =document.querySelector('.text1').innerHTML += ` <br>
+  <input type="text" name="inputField" placeholder="show" id="text2"  >
+  <p> show me ! </p>
+ `
+let text_select = document.querySelector('#text2')
+let paragraph = document.querySelector('.text1>p')
+text_select.setAttribute("value", paragraph.textContent)
+document.querySelector("body").innerHTML+=`<hr>`
+
+// loop use
+let lis= document.querySelectorAll(".listLoop>li")
+lis.forEach(element => { // for(let i = 0;i<lis.length;i++)
+   console.log(element.textContent)  // clg(lis[i].textContent)
+});
+// Dynamic DOM
+// create a element
+var dy=document.createElement("h1")
+dy.textContent=("Dynamic Dom")
+dy.style.color="yellow"
+document.querySelector("body").append(dy) // last child add
+// .prepend() is add first 
+
+
+// css class 
+dy.classList.add("dynamic")
+dy.classList.remove("dynamic")
+dy.classList.toggle("dynamic") // if exits then remove if not exits then add
+
+
+
